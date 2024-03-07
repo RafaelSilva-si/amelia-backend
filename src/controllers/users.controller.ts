644e435c06d7150be07a32fc7c2992d49a1b0ee4
Rafael Silva <rafaelsilva@rafael>
2024-03-07 08:56:43 -0300

    feat: cria controller para user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,71 @@
+import { NextFunction, Response } from 'express';
+
+import { CreateUserDto, UpdateUserDto } from '../dtos/users.dto';
+import UserService from '../services/users.service';
+import { RequestWithUser, User } from '../interfaces/index.interface';
+
 class UsersController {
-  public async findAllUsers() {
-    return "This action returns all users";
-  }
+  private usersService = new UserService();
+
+  public findAllUsers = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const filterParams: any = req.body;
+      const userReqData: User = req.user;
+      const findAllUsersData = await this.usersService.findAllUser({ ...filterParams, deletedAt: null }, userReqData);
+
+      res.status(200).json({ data: findAllUsersData, message: 'OK' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public findUserByEmail = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const userEmail: string = req.body.email;
+      const userReqData: User = req.user;
+      const findUserData: User = await this.usersService.findUserByEmail(userEmail, userReqData);
+
+      res.status(200).json({ data: findUserData, message: 'OK' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public createUser = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const userData: CreateUserDto = req.body;
+      const userReqData: User = req.user;
+      const createUserData: User = await this.usersService.createUser(userData, userReqData);
+
+      res.status(201).json({ data: createUserData, message: 'created' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public updateUser = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const userData: UpdateUserDto = req.body;
+      const userReqData: User = req.user;
+      const updateUserData: User = await this.usersService.updateUser(userData, userReqData);
+
+      res.status(200).json({ data: updateUserData, message: 'updated' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public deleteUser = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const userId: string = req.body._id;
+      const userReqData: User = req.user;
+      const deleteUserData: User = await this.usersService.deleteUser(userId, userReqData);
+
+      res.status(200).json({ data: deleteUserData, message: 'deleted' });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default UsersController;
